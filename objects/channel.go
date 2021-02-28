95afb4e067f492cb7b2f26324a234676f7706631
Emzy <amiamemetoo@gmail.com>
2021-02-28 16:38:03 +0100

    Added emoji structure

--- objects/channel.go
+++ objects/channel.go
@@ -5,13 +5,25 @@ import "time"
 // Snowflake discord ID
 type Snowflake string
 
+// ChannelType Specifies the Channel Type
+type ChannelType int
+
+// Specifies the Channel Types
+const (
+	GuildText     ChannelType = 0
+	DM            ChannelType = 1
+	GuildVoice    ChannelType = 2
+	GroupDM       ChannelType = 3
+	GuildCategory ChannelType = 4
+	GuildNews     ChannelType = 5
+	GuildStore    ChannelType = 6
+)
 
 // Channel a channel object
 type Channel struct {
 	// ID channel ID
-	ID Snowflake `json:"id"`
-	// Type channel type
-	TypeInt uint8 `json:"type"`
+	ID      Snowflake   `json:"id"`
+	TypeInt ChannelType `json:"type"`
 	// GuildID server ID
 	GuildID Snowflake `json:"guild_id,omitempty"`
 
@@ -36,25 +48,4 @@ type Channel struct {
 func (t Timestamp) ToReadableTime() string {
 	ts, _ := time.Parse("2006-01-02T15:04:05", string(t))
 	return ts.String()
-}
-
-// Type turns TypeInt to string
-func (c Channel) Type() string {
-	switch c.TypeInt {
-	case 0:
-		return "text"
-	case 1:
-		return "dm"
-	case 2:
-		return "voice"
-	case 3:
-		return "group"
-	case 4:
-		return "category"
-	case 5:
-		return "news"
-	case 6:
-		return "store"
-	}
-	return ""
 }
